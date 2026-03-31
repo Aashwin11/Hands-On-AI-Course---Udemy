47c09be3ff2ca2cd602c564583aabf96e689c1a7
ANIKET RITAM <140789442+Aashwin11@users.noreply.github.com>
2026-03-31 14:54:09 +0000

    Adding Section 12

--- Section 11 MultiThreading,Multiprocessing, GIL/78-thread-and-concurrency-1.py
+++ Section 11 MultiThreading,Multiprocessing, GIL/78-thread-and-concurrency-1.py
@@ -1,1 +1,40 @@
 # Section 11 MultiThreading,Multiprocessing, GIL
+#thread and concurrency
+
+#Concurrency - Multiple task at once
+#Parallelism - Multiple task at once at the same time,
+
+#Concurrency Example:
+
+#1. Thread Concurrency
+
+import threading
+import time
+
+def take_orders():
+
+    for i in range(1,4):
+        print(f"Taking order: #{i}..... ")
+        time.sleep(2) #1 is second
+
+def brew_chai():
+    for i in range(1,4):
+        print(f"Brewing chai for order: #{i}....")
+        time.sleep(3)
+
+
+#Create threads
+take_order_thread=threading.Thread(target=take_orders) #Thead should have some target, like a function
+brew_thread=threading.Thread(target=brew_chai) #Thead should have some target, like a function
+#Threads are only created and know the target, we need to invoke
+
+take_order_thread.start()
+brew_thread.start()
+
+
+# wait for both to finish
+
+take_order_thread.join()
+brew_thread.join()
+
+print("Process completed")
