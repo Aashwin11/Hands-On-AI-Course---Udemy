2d9793b22f9c296dbcb1dd53926dcc8c1559f14a
ANIKET RITAM <140789442+Aashwin11@users.noreply.github.com>
2026-04-05 14:13:35 +0000

    Working Section12

---
+++ Section 12 Asyncio/87-non-daemon-1.py
@@ -0,0 +1,11 @@
+import threading,time
+
+def monitor_tea_temp():
+    while True:
+        print(f"Monitoring the tea Temp...")
+        time.sleep(2)
+
+
+threading.Thread(target=monitor_tea_temp,daemon=False).start()
+
+print("MAin program ended")
